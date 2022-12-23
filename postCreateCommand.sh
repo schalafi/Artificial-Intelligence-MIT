b241ec1be09d4d56d099bf361a38f8858257c089
schalafi <118856089+schalafi@users.noreply.github.com>
2022-12-23 04:50:54 +0000

    update post create command

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
 sudo apt install python3-pip
-#latex 
+# latex 
 sudo apt install texlive texlive-latex-extra
 pip3 install manim
